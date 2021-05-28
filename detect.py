ef052f04ab48bf82bd160f46f96a63de64faf571
Faris <afra.faris123@gmail.com>
2021-05-28 10:16:23 +0700

    def

--- detect.py
+++ detect.py
@@ -4,5 +4,8 @@ import cv2
 import math
 import argparse
 
-
-   s
+def highlightFace(net, frame, conf_threshold=0.7):
+    frameOpencvDnn=frame.copy()
+    frameHeight=frameOpencvDnn.shape[0]
+    frameWidth=frameOpencvDnn.shape[1]
+    blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
